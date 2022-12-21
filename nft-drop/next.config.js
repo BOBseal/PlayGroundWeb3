277b061863e8d56a501f3e4b34ed54f4e8c0f86f
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2022-12-21 20:56:08 +0530

    rearrange old repo

--- nft-drop/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["https://gateway.ipfscdn.io/ipfs/"],
-  },
-}
-
-module.exports = nextConfig
--- nft-drop/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-
-import creator from "./creator"
-import collection from "./collection"
-
-export const schemaTypes = [creator , collection]
